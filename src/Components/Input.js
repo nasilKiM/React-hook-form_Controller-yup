7e58cc1935dbf4fdeee2a2ecf1066902a35b61c5
nasilKiM <nasikun@naver.com>
2023-07-15 23:04:49 +0900

    :memo: Init : login react-hook-form

---
+++ src/Components/Input.js
@@ -0,0 +1,48 @@
+import { Controller } from "react-hook-form";
+import styled from "styled-components";
+
+const ControlledInput = ({ name, control, defaultValue, ...rest }) => {
+  const controllerProps = {
+    name,
+    defaultValue,
+    control,
+  };
+
+  return (
+    <Controller
+      {...controllerProps}
+      render={({ field: { onBlur, onChange, value }, fieldState: { error } }) => {
+        return (
+          <>
+            <CustomInput
+              name={name}
+              {...rest}
+              hasError={!!error}
+              value={value}
+              onBlur={onBlur}
+              onChange={onChange}
+            />
+            {error && <ErrorMessage>{error.message}</ErrorMessage>}{" "}
+          </>
+        );
+      }}
+    ></Controller>
+  );
+};
+
+export default ControlledInput;
+
+const CustomInput = styled.input`
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  border: 2px solid ${(props) => (props.hasError ? "red" : "black")};
+  padding: 10px;
+  margin-bottom: 10px;
+`;
+
+const ErrorMessage = styled.div`
+  color: red;
+  font-size: 12px;
+  margin: 10px;
+`;
---
+++ src/Page/login.js
@@ -0,0 +1,75 @@
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import ControlledInput from "../Components/Input";
+import styled from "styled-components";
+
+const schema = yup.object().shape({
+  email: yup.string().email().required("이메일을 입력해주세요"),
+  password: yup
+    .string()
+    .required("비밀번호를 입력해주세요")
+    .min(8, "비밀번호는 최소 8자 이상이어야 합니다"),
+});
+
+const LoginHookForm = () => {
+  const {
+    control,
+    handleSubmit,
+    trigger,
+    formState: { isValid },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "all", //제출하기 전
+    reValidateMode: "onChange", //제출하고 나서
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  useEffect(() => {
+    trigger(); //유효성 검사 강제로 시작하게 함
+  }, []);
+
+  const onSubmit = (info) => console.log(info);
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <ControlledInput
+        control={control}
+        name={"email"}
+        type="email"
+        label={"이메일"}
+        placeholder={"이메일을 입력해주세요"}
+      />
+      <ControlledInput
+        control={control}
+        name={"password"}
+        type="password"
+        label={"비밀번호"}
+        placeholder={"비밀번호를 입력해주세요"}
+      />
+      <CustomButton type="submit" disabled={!isValid}>
+        로그인
+      </CustomButton>
+    </Form>
+  );
+};
+
+export default LoginHookForm;
+
+const Form = styled.div`
+  width: 100%;
+`;
+
+const CustomButton = styled.button`
+  width: 100px;
+  padding: 7px;
+  border: none;
+  background-color: gray;
+  font-weight: 600;
+  color: white;
+  cursor: pointer;
+`;
